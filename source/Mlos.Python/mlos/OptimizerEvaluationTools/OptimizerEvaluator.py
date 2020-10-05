fa276de9d8767932c9482378c0ae8eb0ac3bd12e
Adam Śmiechowski <adsmiech@microsoft.com>
2020-10-05 14:26:37 -0700

    Making optimizer evaluator stateful

--- source/Mlos.Python/mlos/OptimizerEvaluationTools/OptimizerEvaluator.py
+++ source/Mlos.Python/mlos/OptimizerEvaluationTools/OptimizerEvaluator.py
@@ -4,12 +4,14 @@
 #
 import pickle
 import mlos.global_values
+from mlos.OptimizerEvaluationTools.ObjectiveFunctionBase import ObjectiveFunctionBase
 from mlos.OptimizerEvaluationTools.ObjectiveFunctionFactory import ObjectiveFunctionFactory, objective_function_config_store
 from mlos.OptimizerEvaluationTools.OptimizerEvaluationReport import OptimizerEvaluationReport
 from mlos.OptimizerEvaluationTools.OptimizerEvaluatorConfigStore import optimizer_evaluator_config_store
 from mlos.OptimizerEvaluationTools.OptimumOverTime import OptimumOverTime
 from mlos.Optimizers.BayesianOptimizerFactory import BayesianOptimizerFactory, bayesian_optimizer_config_store
 from mlos.Optimizers.OptimizationProblem import OptimizationProblem, Objective
+from mlos.Optimizers.OptimizerBase import OptimizerBase
 from mlos.Optimizers.OptimumDefinition import OptimumDefinition
 from mlos.Optimizers.RegressionModels.GoodnessOfFitMetrics import DataSetType
 from mlos.Optimizers.RegressionModels.RegressionModelFitState import RegressionModelFitState
@@ -33,121 +35,164 @@ class OptimizerEvaluator:
 
     """
 
-    @classmethod
-    @trace()
-    def evaluate_optimizer(
-        cls,
+    def __init__(
+        self,
         optimizer_evaluator_config: Point,
-        optimizer_config: Point,
-        objective_function_config: Point
-    ) -> OptimizerEvaluationReport:
-
+        optimizer: OptimizerBase = None,
+        optimizer_config: Point = None,
+        objective_function: ObjectiveFunctionBase = None,
+        objective_function_config: Point = None,
+    ):
         assert optimizer_evaluator_config in optimizer_evaluator_config_store.parameter_space
-        assert optimizer_config in bayesian_optimizer_config_store.parameter_space
-        assert objective_function_config in objective_function_config_store.parameter_space
+        assert (optimizer is None) != (optimizer_config is None), "A valid optimizer XOR a valid optimizer_config must be supplied."
+        assert (objective_function is None) != (objective_function_config is None),\
+            "A valid objective_function XOR a valid objective_function_config must be specified"
+
+        # Let's get the objective function assigned to self's fields.
+        #
+        if (objective_function_config is not None) and (objective_function is None):
+            assert objective_function_config in objective_function_config_store.parameter_space
+            self.objective_function_config = objective_function_config
+            self.objective_function = ObjectiveFunctionFactory.create_objective_function(objective_function_config)
+
+        elif (objective_function is not None) and (objective_function_config is None):
+            self.objective_function_config = objective_function.objective_function_config
+            self.objective_function = objective_function
+
+        else:
+            # The assert above should have caught it but just in case someone removes or changes it.
+            #
+            assert False, "A valid objective_function XOR a valid objective_function_config must be specified"
+
+        # Let's get the optimzier and its config assigned to self's fields.
+        #
+        if (optimizer_config is not None) and (optimizer is None):
+            assert optimizer_config in bayesian_optimizer_config_store.parameter_space
+
+            objective_name = objective_function.output_space.dimension_names[0]
+            optimization_problem = OptimizationProblem(
+                parameter_space=objective_function.parameter_space,
+                objective_space=objective_function.output_space,
+                objectives=[Objective(name=objective_name, minimize=True)]
+            )
+
+            self.optimizer_config = optimizer_config
+            self.optimizer = BayesianOptimizerFactory().create_local_optimizer(
+                optimizer_config=optimizer_config,
+                optimization_problem=optimization_problem
+            )
+
+        elif (optimizer is not None) and (optimizer_config is None):
+            # TODO: assert that the optimization problem in the optimizer matches the objective function.
+            # But this requires Hypergrid.__eq__.
+            #
+            self.optimizer_config = optimizer.optimizer_config
+            self.optimizer = optimizer
+
+        else:
+            # Again, the assert at the beginning of the constructor should have caught this. But more asserts => less bugs.
+            #
+            assert False, "A valid optimizer XOR a valid optimizer_config must be supplied."
+
+
+    @trace()
+    def evaluate_optimizer(self) -> OptimizerEvaluationReport:
 
         evaluation_report = OptimizerEvaluationReport(
-            optimizer_configuration=optimizer_config,
-            objective_function_configuration=objective_function_config,
+            optimizer_configuration=self.optimizer_config,
+            objective_function_configuration=self.objective_function_config,
         )
 
-        if optimizer_evaluator_config.include_execution_trace_in_report:
+        if self.optimizer_evaluator_config.include_execution_trace_in_report:
             mlos.global_values.declare_singletons()
             if mlos.global_values.tracer is None:
                 mlos.global_values.tracer = Tracer()
             mlos.global_values.tracer.clear_events()
 
-        optimizer_factory = BayesianOptimizerFactory()
-        objective_function = ObjectiveFunctionFactory.create_objective_function(objective_function_config)
 
-        if optimizer_evaluator_config.include_pickled_objective_function_in_report:
-            evaluation_report.pickled_objective_function_initial_state = pickle.dumps(objective_function)
+        if self.optimizer_evaluator_config.include_pickled_objective_function_in_report:
+            evaluation_report.pickled_objective_function_initial_state = pickle.dumps(self.objective_function)
 
-        objective_name = objective_function.output_space.dimension_names[0]
-        optimization_problem = OptimizationProblem(
-            parameter_space=objective_function.parameter_space,
-            objective_space=objective_function.output_space,
-            objectives=[Objective(name=objective_name, minimize=True)]
-        )
-
-        optimizer = optimizer_factory.create_local_optimizer(
-            optimizer_config=optimizer_config,
-            optimization_problem=optimization_problem
-        )
-
-        if optimizer_evaluator_config.include_pickled_optimizer_in_report:
-            evaluation_report.pickled_optimizer_initial_state = pickle.dumps(optimizer)
+        if self.optimizer_evaluator_config.include_pickled_optimizer_in_report:
+            evaluation_report.pickled_optimizer_initial_state = pickle.dumps(self.optimizer)
 
         regression_model_fit_state = RegressionModelFitState()
 
         optima_over_time = {}
         optima_over_time["best_observation"] = OptimumOverTime(
-            optimization_problem=optimization_problem,
+            optimization_problem=self.optimizer.optimization_problem,
             optimum_definition=OptimumDefinition.BEST_OBSERVATION
         )
 
         optima_over_time["best_predicted_value"] = OptimumOverTime(
-            optimization_problem=optimization_problem,
+            optimization_problem=self.optimizer.optimization_problem,
             optimum_definition=OptimumDefinition.PREDICTED_VALUE_FOR_OBSERVED_CONFIG
         )
 
         optima_over_time["ucb_99"] = OptimumOverTime(
-            optimization_problem=optimization_problem,
+            optimization_problem=self.optimizer.optimization_problem,
             optimum_definition=OptimumDefinition.UPPER_CONFIDENCE_BOUND_FOR_OBSERVED_CONFIG,
             alpha=0.01
         )
 
         optima_over_time["lcb_99"] = OptimumOverTime(
-            optimization_problem=optimization_problem,
+            optimization_problem=self.optimizer.optimization_problem,
             optimum_definition=OptimumDefinition.LOWER_CONFIDENCE_BOUND_FOR_OBSERVED_CONFIG,
             alpha=0.01
         )
 
         #####################################################################################################
 
-        for i in range(optimizer_evaluator_config.num_iterations):
-            parameters = optimizer.suggest()
-            objectives = objective_function.evaluate_point(parameters)
-            optimizer.register(parameters.to_dataframe(), objectives.to_dataframe())
+        for i in range(self.optimizer_evaluator_config.num_iterations):
+            parameters = self.optimizer.suggest()
+            objectives = self.objective_function.evaluate_point(parameters)
+            self.optimizer.register(parameters.to_dataframe(), objectives.to_dataframe())
 
-            if i % optimizer_evaluator_config.evaluation_frequency == 0:
-                print(f"[{i+1}/{optimizer_evaluator_config.num_iterations}]")
-                if optimizer.trained:
-                    gof_metrics = optimizer.compute_surrogate_model_goodness_of_fit()
+            if i % self.optimizer_evaluator_config.evaluation_frequency == 0:
+                print(f"[{i+1}/{self.optimizer_evaluator_config.num_iterations}]")
+                if self.optimizer.trained:
+                    gof_metrics = self.optimizer.compute_surrogate_model_goodness_of_fit()
                     regression_model_fit_state.set_gof_metrics(data_set_type=DataSetType.TRAIN, gof_metrics=gof_metrics)
 
                 for optimum_name, optimum_over_time in optima_over_time.items():
                     try:
-                        optimum_config, optimum_value = optimizer.optimum(optimum_definition=optimum_over_time.optimum_definition,alpha=optimum_over_time.alpha)
+                        optimum_config, optimum_value = self.optimizer.optimum(
+                            optimum_definition=optimum_over_time.optimum_definition,
+                            alpha=optimum_over_time.alpha
+                        )
                         optima_over_time[optimum_name].add_optimum_at_iteration(iteration=i, optimum_config=optimum_config, optimum_value=optimum_value)
                     except ValueError as e:
                         print(e)
 
-        if optimizer.trained:
-            gof_metrics = optimizer.compute_surrogate_model_goodness_of_fit()
+        if self.optimizer.trained:
+            gof_metrics = self.optimizer.compute_surrogate_model_goodness_of_fit()
             regression_model_fit_state.set_gof_metrics(data_set_type=DataSetType.TRAIN, gof_metrics=gof_metrics)
 
         for optimum_name, optimum_over_time in optima_over_time.items():
             try:
-                optimum_config, optimum_value = optimizer.optimum(optimum_definition=optimum_over_time.optimum_definition, alpha=optimum_over_time.alpha)
-                optima_over_time[optimum_name].add_optimum_at_iteration(iteration=optimizer_evaluator_config.num_iterations, optimum_config=optimum_config, optimum_value=optimum_value)
+                optimum_config, optimum_value = self.optimizer.optimum(optimum_definition=optimum_over_time.optimum_definition, alpha=optimum_over_time.alpha)
+                optima_over_time[optimum_name].add_optimum_at_iteration(
+                    iteration=self.optimizer_evaluator_config.num_iterations,
+                    optimum_config=optimum_config,
+                    optimum_value=optimum_value
+                )
             except Exception as e:
                 print(e)
 
-        if optimizer_evaluator_config.include_execution_trace_in_report:
+        if self.optimizer_evaluator_config.include_execution_trace_in_report:
             evaluation_report.execution_trace = mlos.global_values.tracer.trace_events
             mlos.global_values.tracer.clear_events()
 
-        if optimizer_evaluator_config.include_pickled_optimizer_in_report:
-            evaluation_report.pickled_optimizer_final_state = pickle.dumps(optimizer)
+        if self.optimizer_evaluator_config.include_pickled_optimizer_in_report:
+            evaluation_report.pickled_optimizer_final_state = pickle.dumps(self.optimizer)
 
-        if optimizer_evaluator_config.include_pickled_objective_function_in_report:
-            evaluation_report.pickled_objective_function_final_state = pickle.dumps(objective_function)
+        if self.optimizer_evaluator_config.include_pickled_objective_function_in_report:
+            evaluation_report.pickled_objective_function_final_state = pickle.dumps(self.objective_function)
 
-        if optimizer_evaluator_config.report_regression_model_goodness_of_fit:
+        if self.optimizer_evaluator_config.report_regression_model_goodness_of_fit:
             evaluation_report.regression_model_goodness_of_fit_state=regression_model_fit_state
 
-        if optimizer_evaluator_config.report_optima_over_time:
+        if self.optimizer_evaluator_config.report_optima_over_time:
             evaluation_report.optima_over_time = optima_over_time
 
         return evaluation_report
