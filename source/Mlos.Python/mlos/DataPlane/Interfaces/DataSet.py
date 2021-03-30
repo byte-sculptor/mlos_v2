ae7720c3380e6318e815e12311ccdb77fe36d483
Adam Smiechowski <a.h.smiechowski@gmail.com>
2021-03-29 20:52:16 -0700

    Defined the interfaces for DataSet and DataSetView

--- source/Mlos.Python/mlos/DataPlane/Interfaces/DataSet.py
+++ source/Mlos.Python/mlos/DataPlane/Interfaces/DataSet.py
@@ -2,7 +2,20 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 #
-from abc import ABC
+from abc import ABC, abstractmethod
+import pandas as pd
 
-class DataSet(ABC):
+from mlos.DataPlane.Interfaces.DataSetInfo import DataSetInfo
+from mlos.DataPlane.Interfaces.DataSetView import DataSetView
+from mlos.Spaces import Hypergrid
+
+class DataSet(DataSetView):
     """"""
+
+    @abstractmethod
+    def set_dataframe(self, df: pd.DataFrame) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def append_dataframe(self, df: pd.DataFrame) -> None:
+        raise NotImplementedError
--- source/Mlos.Python/mlos/DataPlane/Interfaces/DataSetView.py
+++ source/Mlos.Python/mlos/DataPlane/Interfaces/DataSetView.py
@@ -2,9 +2,25 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 #
-from abc import ABC
+from abc import ABC, abstractmethod
+import pandas as pd
+
+from mlos.DataPlane.Interfaces.DataSetInfo import DataSetInfo
+from mlos.Spaces import Hypergrid
+
 
 class DataSetView(ABC):
-    """Defines an interface to immutable views of the data set.
+    """"""
 
-    """
+    @property
+    @abstractmethod
+    def schema(self) -> Hypergrid:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_dataframe(self) -> pd.DataFrame:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_data_set_info(self) -> DataSetInfo:
+        raise NotImplementedError
