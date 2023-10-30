79f028a2f964ac50882fb187340b950e3cd0236c
Adam Smiechowski <adam.smiechowski@10xgenomics.com>
2023-10-30 09:11:53 -0700

    Work on tests.

--- source/Mlos.Python/mlos/Spaces/Constraints/TestConstraints.py
+++ source/Mlos.Python/mlos/Spaces/Constraints/TestConstraints.py
@@ -2,6 +2,10 @@
 
 import pytest
 
+from mlos.OptimizerEvaluationTools.ObjectiveFunctionFactory import (
+    ObjectiveFunctionFactory,
+    objective_function_config_store
+)
 from mlos.Spaces.Constraints.Constraint import Constraint
 from mlos.Spaces.SimpleHypergrid import SimpleHypergrid
 
@@ -16,6 +20,30 @@ class TestConstraints:
         4. Constraints on Adapters
     """
 
+    @classmethod
+    def setup_class(cls):
+        cls.three_level_quadratic = ObjectiveFunctionFactory.create_objective_function(
+            objective_function_config=objective_function_config_store.get_config_by_name(
+                name="three_level_quadratic"
+            )
+        )
+
+        cls.flower = ObjectiveFunctionFactory.create_objective_function(
+            objective_function_config=objective_function_config_store.get_config_by_name(
+                name="flower"
+            )
+        )
+
+        cls.multi_objective_waves = ObjectiveFunctionFactory.create_objective_function(
+            objective_function_config=objective_function_config_store.get_config_by_name(
+                name="multi_objective_waves_3_params_2_objectives_half_pi_phase_difference"
+            )
+        )
+
+
     def test_valid_constraints(self):
         constraint = Constraint()
+
+    def test_constraints_on_adapted_space(self):
+        # TODO: add adapters and make sure they work.
         
